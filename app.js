c7c0a5bdece2f0514573bc293435fcb77c1c7b52
Chris <chris@Chriss-MacBook-Pro.local>
2018-12-28 15:09:56 -0500

    Finally fixed the error.  I had to downgrade my version of MySQL.  Express must not be supported yet on the version I am using? I am not sure

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'root',
+	password: 'root',
+	database: 'db_mka'
+});
+
+db.connect((error) => {
+	if (error)
+		console.log(error);
+	else
+		console.log("Connection established");
+});
+
+const app = express();
+app.listen('8889', () => {
+	console.log("Server started on port 8889");
+});
