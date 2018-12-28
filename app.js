f13fceb65fb35af803acf994f8216e9cb7469f58
Chris <chris@Chriss-MacBook-Pro.local>
2018-12-28 15:13:35 -0500

    Had to make one more small fix because localhost could not be found.  Now we are connecting to MySQL and can see the connection in the browser windown

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-	host: '127.0.0.1',
+//	host: '127.0.0.1',
+	host: 'localhost',
 	user: 'root',
 	password: 'root',
 	database: 'db_mka'
